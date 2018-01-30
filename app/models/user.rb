926c103d0a9a81524a346e13010fe7983f540ddf
samantha <kanineshi@gmail.com>
2018-01-30 06:57:43 +0000

    add user's settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_many :uh_relationships
+  has_many :hashtag , :through => :uh_relationships
 end
